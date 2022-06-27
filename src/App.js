7c4346051721cd541c2ae83615ba4d1bb1c5e1f9
gherghinandrei <andrei.gherghin95@gmail.com>
2022-06-27 23:40:02 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import TinderCard from 'react-tinder-card'
+import { Card, Box, Avatar, Chip, Switch } from '@mui/material'
+import React from 'react'
+
+const onSwipe = (direction) => {
+  console.log('You swiped: ' + direction)
+}
+
+const onCardLeftScreen = (myIdentifier) => {
+  console.log(myIdentifier + ' left the screen')
+}
+
+// const response = await fetch("https://api.themoviedb.org/3/trending/all/day?api_key=360a9b5e0dea438bac3f653b0e73af47&language=en-US");
+// const jsonData = await response.json();
+
+// console.log(jsonData);
+
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <TinderCard
+        onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')}>
+        <Card>
+          <Box sx={{ p: 2, display: 'flex' }}>
+            <Avatar variant="rounded" src="avatar1.jpg" />
+          </Box>
+          <Chip>Active account</Chip>
+          <Switch />
+        </Card>
+      </TinderCard>
+    );
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header center-screen">
+        {/* {data.map((user) => ( */}
+        <MovieCard />
+        {/* ))} */}
       </header>
-    </div>
+    </div >
   );
 }
 
